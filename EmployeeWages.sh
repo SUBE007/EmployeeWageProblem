f4cb4c55e7d437db3baa45859865fbd0a8c5576b
SUBE007 <chaurasiyasube007@gmail.com>
2020-03-09 13:37:35 +0530

    [sube007] refactored previous empwage using Function emphr

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -9,18 +9,30 @@ totalworkingday=20
 limithr=100
 numberOfWorkingHr=0
 day=1
+
+function getWorkHr()
+{
+  number=$((RANDOM%3))
+ case $number in
+ 	$isFullTime)   emphr=8 ;;
+ 	$isPartTime)   emphr=4 ;;
+	*)	       emphr=0 ;;
+ esac
+return $emphr
+}
+getWorkHr
+emphr=$? 
+
 while (( (($numberOfWorkingHr<$limithr)) && (($day<$totalworkingday)) ))
 do
-number=$((RANDOM%3))
-case $number in
- $isFullTime)	emphr=8 ;;
- $isPartTime)   emphr=4 ;;
-           *)	emphr=0 ;;
-esac
-numberOfWorkingHr=$(($numberOfWorkingHr+$emphr))
-((day++))
-salary=$(($WagePerHr*$emphr))
-totalsalary=$(($totalsalary+$salary))
+	getWorkHr
+	emphr=$?
+
+ numberOfWorkingHr=$(($numberOfWorkingHr+$emphr))
+ ((day++))
+ salary=$(($WagePerHr*$emphr))
+ totalsalary=$(($totalsalary+$salary))
+
 done
 echo "TotalSalary: $totalsalary"
 
