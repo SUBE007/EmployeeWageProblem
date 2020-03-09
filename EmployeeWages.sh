e2accbd9bbccefbf0c409d76c571b31a2684d7fa
SUBE007 <58814742+SUBE007@users.noreply.github.com>
2020-03-09 15:05:57 +0530

    stored in array daily wages

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -9,20 +9,40 @@ totalworkingday=20
 limithr=100
 numberOfWorkingHr=0
 day=1
+count=0
+function getWorkHr()
+{
+  number=$((RANDOM%3))
+ case $number in
+        $isFullTime)   emphr=8 ;;
+        $isPartTime)   emphr=4 ;;
+        *)             emphr=0 ;;
+ esac
+return $emphr
+}
+
+function dwage()
+{
+  local wage=$1
+       eachwage=$(($WagePerHr*$wage))
+return $eachwage
+}
+
 while (( (($numberOfWorkingHr<$limithr)) && (($day<$totalworkingday)) ))
 do
-number=$((RANDOM%3))
-case $number in
- $isFullTime)	emphr=8 ;;
- $isPartTime)   emphr=4 ;;
-           *)	emphr=0 ;;
-esac
-numberOfWorkingHr=$(($numberOfWorkingHr+$emphr))
-((day++))
-salary=$(($WagePerHr*$emphr))
-totalsalary=$(($totalsalary+$salary))
+        getWorkHr
+        emphr=$?
+       dwage $emphr
+         dailywagevalue=$?
+     dailyWage[ ((count++)) ]=$dailywagevalue
+
+ numberOfWorkingHr=$(($numberOfWorkingHr+$emphr))
+ ((day++))
+ salary=$(($WagePerHr*$emphr))
+ totalsalary=$(($totalsalary+$salary))
+
 done
 echo "TotalSalary: $totalsalary"
-
-
+echo "each day wage: ${dailyWage[@]}"
+echo "total Number of wage: ${#dailyWage[@]}"
 
