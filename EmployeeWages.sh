2355ead6a826a0d85a70d39a398b37a4e15f6115
SUBE007 <chaurasiyasube007@gmail.com>
2020-03-09 12:01:15 +0530

    [sube007] refactored EmployeeWages Employee present or not

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 echo "========Welcome to employee wage calculation========= "
+number=$((RANDOM%2))
 
+if(($number==1))
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
