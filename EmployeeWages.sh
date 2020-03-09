525ba53e3c79a7aa3a04f441ec33d2fbb5b203bc
SUBE007 <chaurasiyasube007@gmail.com>
2020-03-09 12:29:53 +0530

    [sube007] refactored previous using case Statement

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -4,15 +4,11 @@ number=$((RANDOM%3))
 isFullTime=2
 isPartTime=1
 WagePerHr=20
-if(($number==$isFullTime))
- then
-        emphr=8
-elif(($number==$isPartTime))
- then
-        emphr=4
-else
-        emphr=0
-fi
+case $number in
+ $isFullTime)	emphr=8 ;;
+ $isPartTime)   emphr=4 ;;
+           *)	emphr=0 ;;
+esac
 salary=$(($WagePerHr*$emphr))
 echo "Salary: $salary"
 
