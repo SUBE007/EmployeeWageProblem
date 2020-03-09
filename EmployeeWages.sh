31c9cc5148f0550355a31ec87d173c0614e348ab
SUBE007 <chaurasiyasube007@gmail.com>
2020-03-09 16:05:26 +0530

    [sube007] refactored using Dictionary

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -9,20 +9,43 @@ totalworkingday=20
 limithr=100
 numberOfWorkingHr=0
 day=1
+count=0
+declare -A dailyWage
+function getWorkHr()
+{
+  number=$((RANDOM%3))
+ case $number in
+ 	$isFullTime)   emphr=8 ;;
+ 	$isPartTime)   emphr=4 ;;
+	*)	       emphr=0 ;;
+ esac
+return $emphr
+}
+
+function dwage()
+{
+  local wage=$1
+       eachwage=$(($WagePerHr*$wage))
+return $eachwage
+}
+
 while (( (($numberOfWorkingHr<$limithr)) && (($day<$totalworkingday)) ))
 do
-number=$((RANDOM%3))
-case $number in
- $isFullTime)	emphr=8 ;;
- $isPartTime)   emphr=4 ;;
-           *)	emphr=0 ;;
-esac
-numberOfWorkingHr=$(($numberOfWorkingHr+$emphr))
-((day++))
-salary=$(($WagePerHr*$emphr))
-totalsalary=$(($totalsalary+$salary))
+	getWorkHr
+	  emphr=$?
+       dwage $emphr
+          dailywagevalue=$?
+     dailyWage["day $day"]=$dailywagevalue
+
+    numberOfWorkingHr=$(($numberOfWorkingHr+$emphr))
+ ((day++))
+ salary=$(($WagePerHr*$emphr))
+ totalsalary=$(($totalsalary+$salary))
+
 done
 echo "TotalSalary: $totalsalary"
+echo "each day wage: ${dailyWage[@]}"
+echo "each day wage: ${!dailyWage[@]}"
 
-
+echo "total Number of wage: ${#dailyWage[@]}"
 
